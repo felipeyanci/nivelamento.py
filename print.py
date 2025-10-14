fb9b8d3018341f583dd60d36578c577bf4d08c4c
felipeyanci <henriquesfelipesousa@gmail.com>
2025-10-13 21:28:37 -0300

    Add files via upload

---
+++ print.py
@@ -0,0 +1,19 @@
+'''print("Felipe Henriques")
+print("Olá mundo !")
+'''
+'''print("100")
+print("7" , "15")
+'''
+'''print("felipe \n 28")
+'''
+'''soma = 2 + 3
+print(soma)
+'''
+'''print(5+8)
+print("Faça \n sua \n tattoo")
+'''
+'''print(1, 2, 3)
+print("'Python é interessante'")
+'''
+print(f"{2}" x "{1}") 
+print
