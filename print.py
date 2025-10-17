d3b3f3e04e2d3391a100529edeaac42a4675875d
felipeyanci <henriquesfelipesousa@gmail.com>
2025-10-16 23:28:53 -0300

    Update print.py

--- print.py
+++ print.py
@@ -15,5 +15,33 @@ print("Faça \n sua \n tattoo")
 '''print(1, 2, 3)
 print("'Python é interessante'")
 '''
-print(f"{2}" x "{1}") 
-print
+'''print(2*1 , 2*2 , 2*3 , 2*4 , 2*5 , 2*6 , 2*7 , 2*8 , 2*9 , 2*10)
+print("\n ***** \n ***** \n *****")
+'''
+'''i = 0
+while i < 2:
+    print("AprendendoPython")
+    i += 1
+'''
+'''a = 0
+while a < 5:
+    print("quero me tornar programador")
+    a += 1
+'''
+'''print("EU ESTOU APRENDENDO PYTHON")
+numero_decimal = 2,50
+print(numero_decimal)
+'''
+'''print(10-4)
+print("Felipe 🙂")
+'''
+'''Lista = ["Pao" , "Leite" , "Cafe"]
+print(Lista)
+
+nome = "felipe"
+idade = 28
+cidade = "campina grande"
+print(nome)
+print(idade)
+print(cidade)
+'''
