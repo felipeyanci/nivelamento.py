da32717f76dd18cb5120506115f49479a24fd56c
felipeyanci <henriquesfelipesousa@gmail.com>
2025-10-19 20:18:33 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,118 @@
+'''print("Seu nome é Felipe")
+
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+print("Olá {nome} , você tem {idade} anos")
+'''
+'''Cidade = "Campina Grande"
+Estado = "Paraiba"
+print(f"Moro em {Cidade} na {Estado}")
+
+Curso = "Programação em Python"
+print(f"Você escolheu o curso de {Curso}")
+'''
+'''Filme = "ET O Extraterrestre"
+Ano = "1982"
+print(f"O filme {Filme} foi lançado em {Ano}")
+
+soma = (37+64)
+print(soma)
+''' 
+
+'''subtração = (45-19)
+print(subtração)
+
+multiplicação = (12*96)
+print(multiplicação)
+
+divisão = (251//26)
+print(divisão)
+'''
+
+'''numero = int(input("Digite um numero:"))
+dobro = numero * 2 
+print(f"O dobro de {numero} é {dobro}")
+'''
+'''ano = int(input("Que ano você nasceu?"))
+anoatual = 2025
+result = anoatual - ano
+print("Sua idade é:" , result)
+'''
+'''dias = 10
+horas = dias * 24
+print(f"{dias} dias é equivalente à {horas} horas")
+'''
+'''numero1 = int(input("Digite um numero:"))
+numero2 = int(input("Digite outro numero:"))
+if numero1 > numero2:
+    print(f"O maior numero é {numero1}")
+elif numero1 > numero2:
+    print(f"O maior numero é {numero2}")
+'''
+
+'''num1 = int(input("Digite um numero:"))
+num2 = int(input("Digite outro numero:"))
+if num1 < num2:
+    print(f"O menor numero é {num1}")
+else:
+    print(f"O menor numero é {num2}")
+'''
+'''num1 = int(input("Digite o primeiro numero:"))
+num2 = int(input("Digite o segundo numero:"))
+num3 = int(input("Digite o terceiro numero:"))
+if num1 >= num2 and num1 >= num3:
+    maior_numero = num1
+elif num2 >= num1 and num2 >= num3:
+    maior_numero = num2
+else:
+    maior_numero = num3
+print(f"O maior numero é {maior_numero}")
+'''
+'''num1 = int(input("Digite o primeiro numero:"))
+num2 = int(input("Digite o segundo numero:"))
+num3 = int(input("Digite o terceiro numero:"))
+soma = num1 + num2 + num3
+media = soma / 3
+print(f"A media dos numeros é:" , media)
+'''
+'''num = int(input("Digite um numero:"))
+if num % 2 == 0:
+    print(f"O numero {num} é par")
+else:
+    print(f"O numero {num} é impar")
+'''
+'''jogador = "wallace"
+time = "flamengo"
+print(f"O jogador {jogador} joga no time do {time}")
+'''
+'''livroatual = "Roube como um artista"
+print(f"Eu estou lendo o livro {livroatual}")
+'''
+'''profissão = "tatuador"
+estudio = "corpus cult"
+print(f"Eu trabalho como {profissão} no estudio da {estudio}")
+'''
+'''animal = "chachorrinha caramelo"
+nome = "frida"
+print(f"meu animal de estimação é uma {animal} e o nome dela é {nome}")
+'''
+'''nome = "felipe"
+comida_favorita = "arroz da terra com galinha"
+print(f"{nome} gosta de comer {comida_favorita}")
+'''
+'''nome = input("Digite o nome de uma banda:")
+musica = input("Digite uma musica:")
+print(f'{nome} toca a musica {musica}')
+'''
+'''cidade = input("Digite uma cidade:")
+ano = int(input("Digite o ano da viagem:"))
+print(f'Visitei {cidade} em {ano}')
+'''
+'''escola = input("Digite o nome da sua escola:")
+curso = input("Digite o curso:")
+print(f'Estudo {curso} na escola {escola}')
+'''
+'''nome = input("Digite o nome de um amigo:")
+apelido = input("Digite um apelido:")
+print(f'Meu amigo {nome} também é conhecido como {apelido}')
+'''
