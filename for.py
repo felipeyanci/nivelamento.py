a13a760adadd80cd4267a7447263d992de1c0030
felipeyanci <henriquesfelipesousa@gmail.com>
2025-10-22 14:15:20 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,49 @@
+'''for i in range(1,11):
+    print(i)
+'''
+
+'''for numero in range(1, 21):
+    if numero % 2 == 0:
+        print(numero)
+'''
+
+'''for i in range(1,11):
+    print(f"5*{i} ={5*i}")
+'''
+
+'''soma = 0
+for i in range(1,101):
+    soma += i
+print(soma)
+'''
+
+'''for numero in range(1,31):
+    if numero % 2 != 0:
+        print(numero)
+'''
+
+'''palavra = "yanci"
+palavra_invertida = palavra[::-1]
+print(f"{palavra_invertida}")
+'''
+
+'''for numero in range(1, 11):
+    if numero % 2 == 0:
+        print(numero)
+'''
+'''numeros = list(range(10))
+print(numeros)
+'''
+
+'''for i in range(10, 0, -1):
+    print(i)
+'''
+
+'''for numero in range(1,51):
+    if numero % 2 == 0:
+        print(numero)
+'''
+
+'''for numero in range(100, 49, -1):
+    print(numero)
+'''
